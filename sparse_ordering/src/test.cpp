69f75e6c0447369c7db1084ffb872e6d3d9894fd
istinj <istinj@gmail.com>
2017-04-14 15:37:34 +0200

    Datasets

--- sparse_ordering/src/test.cpp
+++ sparse_ordering/src/test.cpp
@@ -2,7 +2,10 @@
 #include <Eigen/Core>
 
 #include "defs.h"
+#include "Graph.h"
 
 int main(int argc, char const *argv[]){
+	sparse::Graph* graph = new sparse::Graph();
+	graph->loadFromG2OFile("../data/world_odom_500p_OL.g2o");
 	return 0;
 }
